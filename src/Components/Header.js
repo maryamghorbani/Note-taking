d27a6f347ac3524569fff31012f807c7d3b520ea
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-08-11 13:54:24 +0430

    add header component

---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+
+function Header() {
+
+    return (
+        <header>
+            <div className="main-navbar">
+                <div className="">
+                    <a href="#" className="">
+                        <strong>TODO APP</strong>
+                    </a>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+
+export default Header;
