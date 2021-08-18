0b7da0165da1f655f6e6fd886379627f483f615e
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-08-18 15:31:59 +0430

    add auth file and app Reducer file

---
+++ src/Reducers/appReducer.js
@@ -0,0 +1,106 @@
+function AppReducer(state , action) {
+    console.log(state , action);
+    switch (action.type) {
+        case 'init_note':
+            let { todos } = action.payload;
+            return  {
+                ...state,
+                todos
+            }
+        case 'add_todo':
+            return addNote(state , action);
+        case 'delete_note' :
+            return deleteNote(state,action);
+        case 'edit_note' :
+            return editNote(state , action);
+        case 'login_user' :
+            return {
+                ...state,
+                authenticated : true
+            }
+        case 'logout_user' :
+            return {
+                ...state,
+                authenticated : false
+            }
+        default:
+            return state;
+    }
+}
+
+export default AppReducer;
+
+
+let addNote = (state , action) => {
+    let { note } = action.payload;
+    return {
+        ...state,
+        notes : [
+            ...state.notes,
+            note
+        ]
+    }
+}
+
+let deleteNote = (state , action ) => {
+    let { key } = action.payload;
+
+    return {
+        ...state,
+        notes : state.notes.filter(item => item.key !== key)
+    }
+}
+
+let editNote = (state ,action) => {
+    let { key , text} = action.payload;
+    
+    let item = state.notes.find(item => item.key === key);
+    item.text = text ;
+
+    let newnotes = state.notes.filter(item => item.key !== key)
+
+    return {
+        ...state,
+        notes : [
+            ...newnotes,
+            item
+        ]
+    }
+}
+
+// addNote(text) {
+//     this.setState(prevState => {
+//         return {
+//             notes : [
+//                 ...prevState.notes,
+//                 { key : Date.now() , done : false , text }
+//             ]
+//         }
+//     })
+// }
+
+
+// deleteNote(key) {
+//     this.setState(prevState => {
+//         return {
+//             notes : prevState.notes.filter(item => item.key !== key)
+//         }
+//     })
+// }
+
+// editNote(key , text) {
+//     let { notes } = this.state;
+
+//     let item = notes.find(item => item.key === key);
+//     item.text = text ;
+
+//     let newnotes = notes.filter(item => item.key !== key)
+
+//     this.setState({
+//         notes : [
+//             ...newnotes,
+//             item
+//         ]
+//     })
+// }
+
