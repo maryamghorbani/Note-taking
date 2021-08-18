b8a57e084efb9b2b8ef4fa967dfc43f59cba7f09
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-08-18 16:04:25 +0430

    styled login/logout button

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,14 +1,11 @@
 import React , { useContext } from 'react';
 import './CSS/Header.css';
 import AuthContext from '../../Context/auth';
-import NoteContext from '../../Context/Notes';
 
 
 function Header() {
 
-    const noteContext = useContext(NoteContext);
     const authContext = useContext(AuthContext);
-
 
     let doLogin = () => authContext.dispatch({ type : 'login_user' });
     let doLogout = () => authContext.dispatch({ type : 'logout_user'});
@@ -21,8 +18,8 @@ function Header() {
                 </a>
                     {
                         ! authContext.authenticated
-                            ? <button className="btn btn-sm btn-success" onClick={doLogin}>login</button>
-                            : <button className="btn btn-sm btn-danger" onClick={doLogout}>logout</button>
+                            ? <button className="login-btn" onClick={doLogin}>login</button>
+                            : <button className="logout-btn" onClick={doLogout}>logout</button>
                     }
             </div>
         </header>
