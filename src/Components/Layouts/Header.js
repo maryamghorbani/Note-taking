1ed3a893184ce0b91962d711b5695eee64081037
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-08-18 15:50:11 +0430

    add login button in header

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React , { useContext } from 'react';
 import './CSS/Header.css';
+import AuthContext from '../../Context/auth';
+import NoteContext from '../../Context/Notes';
 
 
 function Header() {
+
+    const noteContext = useContext(NoteContext);
+    const authContext = useContext(AuthContext);
+
+
+    let doLogin = () => authContext.dispatch({ type : 'login_user' });
+    let doLogout = () => authContext.dispatch({ type : 'logout_user'});
 
     return (
         <header>
@@ -10,6 +19,11 @@ function Header() {
                 <a href="#" className="">
                     <strong>TODO APP</strong>
                 </a>
+                    {
+                        ! authContext.authenticated
+                            ? <button className="btn btn-sm btn-success" onClick={doLogin}>login</button>
+                            : <button className="btn btn-sm btn-danger" onClick={doLogout}>logout</button>
+                    }
             </div>
         </header>
     )
