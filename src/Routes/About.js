1dd782bb8e6d920b4efa9c7c77326ccaee1e033a
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-08-21 17:33:52 +0430

    change some directory

--- src/Routes/About.js
+++ src/Routes/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './About.css'
+import './CSS/About.css'
 
 function About(props) {
     return(
--- src/Routes/NotFound.js
+++ src/Routes/NotFound.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './NotFound.css';
+import './CSS/NotFound.css';
 
 function NotFound() {
     return(
